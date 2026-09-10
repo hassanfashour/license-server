a08376e3c4bb634447bb484fe62de0bc1dc05afe
unknown <hassanfashour@gmail.com>
2026-09-10 14:44:57 +0300

    update serial

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ LICENSES = {
     # "SR-TEST-003": {"expiry": "01-01-2027", "used_on": None},
     "SR-TEST-004": {"expiry": "04-04-2027", "used_on": None},
     "SR-TEST-005": {"expiry": "01-09-2026", "used_on": None},
-    "SR-TEST-006": {"expiry": "10-09-2026", "used_on": None},
+    "SR-TEST-006": {"expiry": "10-09-2027", "used_on": None},
 }
 
 @app.route('/')
