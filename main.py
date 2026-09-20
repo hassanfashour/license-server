e2253a412a8108beffc761abce3f9a8e62c1d80e
Hassan F. Ashour <hassanfashour@gmail.com>
2026-09-20 12:03:03 +0300

    Comment out a license key in main.py
    Commented out a license key in the LICENSES dictionary.

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 # ملاحظة: البيانات في القاموس ستتصفّر عند إعادة تشغيل السيرفر على Render
 LICENSES = {
-    "7YGPK-KULE5-FWZDY-5CUDP": {"expiry": "22-09-2026", "used_on": None}, #Hassan
+    # "7YGPK-KULE5-FWZDY-5CUDP": {"expiry": "22-09-2026", "used_on": None}, #Hassan
     "PGXSY-P3NG9-Q22EH-WPQGL": {"expiry": "22-09-2026", "used_on": None}, #Hassan
     "3KC53-64H8G-C2EJ3-V5C7D": {"expiry": "22-09-2026", "used_on": None}, #Hassan
     "5GP2R-SW4GR-AGN9F-KCBGN": {"expiry": "22-09-2026", "used_on": None}, #Hassan
