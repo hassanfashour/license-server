5f874eaaef52c7dda62c6c257781a605972b0721
Hassan F. Ashour <hassanfashour@gmail.com>
2026-09-22 13:55:36 +0300

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ LICENSES = {
     # "PGXSY-P3NG9-Q22EH-WPQGL": {"expiry": "28-09-2026", "used_on": None}, #HBF
     # "3KC53-64H8G-C2EJ3-V5C7D": {"expiry": "28-09-2026", "used_on": None}, #HBF
     "5GP2R-SW4GR-AGN9F-KCBGN": {"expiry": "29-09-2026", "used_on": None}, #HBF
-    "HRTKA-UBJCY-AGTCC-55GMH": {"expiry": "22-09-2026", "used_on": None}, #Hassan
+    "HRTKA-UBJCY-AGTCC-55GMH": {"expiry": "27-09-2026", "used_on": None}, #ISLAM
     "3ZHP8-4CMGH-HEL4L-7J9HN": {"expiry": "22-09-2026", "used_on": None}, #Hassan
     # "SR-TEST-003": {"expiry": "01-01-2027", "used_on": None},
     # "SR-TEST-004": {"expiry": "04-04-2027", "used_on": None},
