c38fd31534e2ab4b427e6cabd509dc1b03f9ed65
Hassan F. Ashour <hassanfashour@gmail.com>
2026-09-24 13:47:40 +0300

    Replace commented code with new key-value pair

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ LICENSES = {
     "5GP2R-SW4GR-AGN9F-KCBGN": {"expiry": "29-09-2026", "used_on": None}, #HBF
     # "HRTKA-UBJCY-AGTCC-55GMH": {"expiry": "27-09-2026", "used_on": None}, #ISLAM
     "3ZHP8-4CMGH-HEL4L-7J9HN": {"expiry": "27-09-2026", "used_on": None}, #ISLAM
-    # "SR-TEST-003": {"expiry": "01-01-2027", "used_on": None},
+    "TVYNH-JPFJ7-29TL4-PTFKQ": {"expiry": "01-10-2026", "used_on": None}, #SaYa
     # "SR-TEST-004": {"expiry": "04-04-2027", "used_on": None},
     # "SR-TEST-005": {"expiry": "01-09-2026", "used_on": None},
     # "SR-TEST-006": {"expiry": "17-09-2027", "used_on": None},
