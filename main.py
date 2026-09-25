dfa544419b075ca5704ca77484035a70e77f7d52
Hassan F. Ashour <hassanfashour@gmail.com>
2026-09-25 16:25:33 +0300

    Modify license expiry dates and update used_on
    Updated expiry dates for existing licenses and modified the used_on field.

--- main.py
+++ main.py
@@ -7,13 +7,10 @@ app = Flask(__name__)
 # ملاحظة: البيانات في القاموس ستتصفّر عند إعادة تشغيل السيرفر على Render
 LICENSES = {
     "7YGPK-KULE5-FWZDY-5CUDP": {"expiry": "22-10-2026", "used_on": None}, #Hassan
-    # "PGXSY-P3NG9-Q22EH-WPQGL": {"expiry": "28-09-2026", "used_on": None}, #HBF
-    # "3KC53-64H8G-C2EJ3-V5C7D": {"expiry": "28-09-2026", "used_on": None}, #HBF
-    "5GP2R-SW4GR-AGN9F-KCBGN": {"expiry": "29-09-2026", "used_on": None}, #HBF
-    # "HRTKA-UBJCY-AGTCC-55GMH": {"expiry": "27-09-2026", "used_on": None}, #ISLAM
+    "5GP2R-SW4GR-AGN9F-KCBGN": {"expiry": "01-10-2026", "used_on": None}, #HBF
     "3ZHP8-4CMGH-HEL4L-7J9HN": {"expiry": "27-09-2026", "used_on": None}, #ISLAM
-    "TVYNH-JPFJ7-29TL4-PTFKQ": {"expiry": "01-10-2026", "used_on": None}, #SaYa
-    "R8FSU-AV4SB-9EUXQ-6GRMP": {"expiry": "24-10-2026", "used_on": None}, #OMER
+    "TVYNH-JPFJ7-29TL4-PTFKQ": {"expiry": "02-10-2026", "used_on": None}, #SaYa
+    "R8FSU-AV4SB-9EUXQ-6GRMP": {"expiry": "26-10-2026", "used_on": None}, #OMER
     # "SR-TEST-005": {"expiry": "01-09-2026", "used_on": None},
     # "SR-TEST-006": {"expiry": "17-09-2027", "used_on": None},
     # "SR-TEST-007": {"expiry": "14-09-2026", "used_on": None},
